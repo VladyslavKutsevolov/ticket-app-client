d181a451fcb1900f19f09da400059c50e406b3da
Vladyslav Kutsevolov <kytsevolov@gmail.com>
2021-04-07 12:26:33 -0600

    define error handler

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,6 +3,7 @@ import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
+import { errorHandler } from "./middlewares/error-handles";
 const app = express();
 
 app.use(express.json());
@@ -11,5 +12,7 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+
+app.use(errorHandler);
 
 app.listen(3000, () => console.log("listen on 3000!"));
