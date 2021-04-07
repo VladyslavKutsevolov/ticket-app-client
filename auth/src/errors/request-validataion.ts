b61b91e62186873d55c2ccd824e011320cd412f5
Vladyslav Kutsevolov <kytsevolov@gmail.com>
2021-04-07 13:58:09 -0600

    added serialize method and statusCode

--- auth/src/errors/request-validataion.ts
+++ auth/src/errors/request-validataion.ts
@@ -1,9 +1,19 @@
 import { ValidationError } from "express-validator";
 
 export class RequestValidataionError extends Error {
+  public statusCode = 400;
   constructor(public errors: ValidationError[]) {
     super();
     //Because we extending build ifn Class
     Object.setPrototypeOf(this, RequestValidataionError.prototype);
+  }
+
+  serialize() {
+    return this.errors.map((e) => {
+      return {
+        message: e.msg,
+        field: e.param,
+      };
+    });
   }
 }
