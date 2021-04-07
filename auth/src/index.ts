76cd1b45bf48c2f18caf6def34a21775bc191660
Vladyslav Kutsevolov <kytsevolov@gmail.com>
2021-04-07 15:11:31 -0600

    if your path not exist throw err

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,13 @@
 import express from "express";
+
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
+
 import { errorHandler } from "./middlewares/error-handles";
+import { NotFoundError } from "./errors/not-found-error";
+
 const app = express();
 
 app.use(express.json());
@@ -13,6 +17,10 @@ app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 
+app.all("*", () => {
+  throw new NotFoundError();
+});
+
 app.use(errorHandler);
 
-app.listen(3000, () => console.log("listen on 3000!"));
+app.listen(3000, () => console.log("listen on 3000"));
