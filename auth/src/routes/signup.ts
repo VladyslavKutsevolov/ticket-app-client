9025429654a40441054cb1109858a7c8486c611f
Vladyslav Kutsevolov <kytsevolov@gmail.com>
2021-04-10 12:12:36 -0600

    validate requests middlware

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,10 +1,10 @@
 import express, { Request, Response } from "express";
-import { body, validationResult } from "express-validator";
-import { RequestValidationError } from "../errors/request-validataion";
+import { body } from "express-validator";
 import jwt from "jsonwebtoken";
 import { BadRequestError } from "../errors/bad-request-error";
 
 import { User } from "../models/user";
+import { validateRequest } from "../middlewares/validate-request";
 
 const router = express.Router();
 
@@ -18,13 +18,8 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage("password should be between 4 an 20"),
   ],
+  validateRequest,
   async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
-    }
-
     const { email, password } = req.body;
     const existingUser = await User.findOne({ email });
 
