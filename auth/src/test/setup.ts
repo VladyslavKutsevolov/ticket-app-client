eae73fdccfb1d1f79ea4a6dc9190a074fc117437
Vladyslav Kutsevolov <kytsevolov@gmail.com>
2021-04-11 13:10:52 -0600

    setup global signin

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,6 +1,15 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
 import { app } from "../app";
+import request from "supertest";
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signin(): Promise<string[]>;
+    }
+  }
+}
 
 let mongo: any;
 beforeAll(async () => {
@@ -27,3 +36,17 @@ afterAll(async () => {
   await mongo.stop();
   await mongoose.connection.close();
 });
+
+global.signin = async () => {
+  const email = "test@test.com";
+  const password = "1234565";
+
+  const res = await request(app)
+    .post("/api/users/signup")
+    .send({ email, password })
+    .expect(201);
+
+  const cookie = res.get("Set-Cookie");
+
+  return cookie;
+};
