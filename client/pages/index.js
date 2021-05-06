bee8ad44a7ddccca2faf92fcb7d7a72275cfba31
Vladyslav Kutsevolov <kytsevolov@gmail.com>
2021-05-06 11:05:21 -0600

    sign in

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,8 +1,7 @@
 import buildClient from "../api/build-client";
 
 const LandingPage = ({ currentUser }) => {
-  console.log("currentUser ", currentUser);
-  return <h1>Landing Page</h1>;
+  return currentUser ? <h1>You are sign in</h1> : <h1>You not are sign in</h1>;
 };
 
 LandingPage.getInitialProps = async (context) => {
