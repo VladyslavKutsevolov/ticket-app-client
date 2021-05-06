e79b7277066dd70a17f9db11023e72ab1c9e4370
Vladyslav Kutsevolov <kytsevolov@gmail.com>
2021-05-06 10:58:25 -0600

    get current user

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,14 @@
-export default () => {
-  return <h1>Hello! </h1>;
+import buildClient from "../api/build-client";
+
+const LandingPage = ({ currentUser }) => {
+  console.log("currentUser ", currentUser);
+  return <h1>Landing Page</h1>;
 };
+
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get("/api/users/currentuser");
+  return data;
+};
+
+export default LandingPage;
