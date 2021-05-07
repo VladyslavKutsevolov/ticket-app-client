243e15185dfca0965dbd5389b367ef134cee02b7
Vladyslav Kutsevolov <kytsevolov@gmail.com>
2021-05-07 17:31:00 -0600

    list all tickets test

---
+++ tickets/src/routes/__test__/index.test.ts
@@ -0,0 +1,21 @@
+import request from "supertest";
+import { app } from "../../app";
+
+const createTicket = () => {
+  return request(app).post("/api/tickets").set("Cookie", global.signin()).send({
+    title: "asda",
+    price: 20,
+  });
+};
+
+describe("Get all tickets", () => {
+  it("should fetch list of tickets", async () => {
+    await createTicket();
+    await createTicket();
+    await createTicket();
+
+    const res = await request(app).get("/api/tickets").send().expect(200);
+
+    expect(res.body.length).toEqual(3);
+  });
+});
