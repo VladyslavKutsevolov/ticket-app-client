376a2e4adadbb8e3d38c62999bb3970ee0641e3d
Vladyslav Kutsevolov <kytsevolov@gmail.com>
2021-05-10 17:04:41 -0600

    test fetch single order for user that owns it

---
+++ orders/src/routes/__test__/show.test.ts
@@ -0,0 +1,51 @@
+import request from "supertest";
+import { app } from "../../app";
+import { Ticket } from "../../models/ticket";
+
+describe("Display single ticket", () => {
+  it("should fetch order", async () => {
+    const userID = global.signin();
+    const ticket = Ticket.build({
+      title: "price",
+      price: 20,
+    });
+
+    await ticket.save();
+
+    const { body: order } = await request(app)
+      .post("/api/orders")
+      .set("Cookie", userID)
+      .send({ ticketId: ticket.id })
+      .expect(201);
+
+    const { body: fetchOrder } = await request(app)
+      .get(`/api/orders/${order.id}`)
+      .set("Cookie", userID)
+      .send()
+      .expect(200);
+
+    expect(fetchOrder.id).toEqual(order.id);
+  });
+
+  it("should return err if one user fetch other user order", async () => {
+    const userID = global.signin();
+    const ticket = Ticket.build({
+      title: "price",
+      price: 20,
+    });
+
+    await ticket.save();
+
+    const { body: order } = await request(app)
+      .post("/api/orders")
+      .set("Cookie", userID)
+      .send({ ticketId: ticket.id })
+      .expect(201);
+
+    await request(app)
+      .get(`/api/orders/${order.id}`)
+      .set("Cookie", global.signin())
+      .send()
+      .expect(401);
+  });
+});
