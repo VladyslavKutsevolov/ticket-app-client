d440c9408c07c5507afb90c685788eb4daf4d161
Vladyslav Kutsevolov <kytsevolov@gmail.com>
2021-05-10 17:11:07 -0600

    delete order

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,9 +1,32 @@
 import express, { Response, Request } from "express";
+import {
+  NotAuthorizedErr,
+  NotFoundError,
+  requireAuth,
+} from "@vladtickets/common";
+import { Order } from "../models/order";
+import { OrderStatus } from "../../../common";
 
 const router = express.Router();
 
-router.delete("/api/orders/:orderId", (req: Request, res: Response) => {
-  res.send({});
-});
+router.delete(
+  "/api/orders/:orderId",
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const order = await Order.findById(req.params.orderId);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedErr();
+    }
+
+    order.status = OrderStatus.Cancelled;
+
+    res.status(204).send(order);
+  }
+);
 
 export { router as deleteOrderRouter };
