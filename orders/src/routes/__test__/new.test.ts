331bc4adc6327a4e3b7cf62812ddcca8fd65090d
Vladyslav Kutsevolov <kytsevolov@gmail.com>
2021-05-10 16:29:40 -0600

    test order creation

---
+++ orders/src/routes/__test__/new.test.ts
@@ -0,0 +1,55 @@
+import mongoose from "mongoose";
+import request from "supertest";
+import { app } from "../../app";
+import { Ticket } from "../../models/ticket";
+import { Order } from "../../models/order";
+import { OrderStatus } from "@vladtickets/common";
+
+describe("Order creation", () => {
+  it("should return err if ticket is not exist", async () => {
+    const ticketId = mongoose.Types.ObjectId();
+    await request(app)
+      .post("/api/orders")
+      .set("Cookie", global.signin())
+      .send({ ticketId })
+      .expect(404);
+  });
+  it("should return err if ticket is already reserved", async () => {
+    const ticket = Ticket.build({
+      title: "concert",
+      price: 20,
+    });
+
+    await ticket.save();
+
+    const order = Order.build({
+      ticket,
+      userId: "vlad",
+      status: OrderStatus.Created,
+      expiresAt: new Date(),
+    });
+
+    await order.save();
+
+    await request(app)
+      .post("/api/orders")
+      .set("Cookie", global.signin())
+      .send({ ticketId: ticket.id })
+      .expect(400);
+  });
+
+  it("should reserve ticket", async () => {
+    const ticket = Ticket.build({
+      title: "concert",
+      price: 20,
+    });
+
+    await ticket.save();
+
+    await request(app)
+      .post("/api/orders")
+      .set("Cookie", global.signin())
+      .send({ ticketId: ticket.id })
+      .expect(201);
+  });
+});
