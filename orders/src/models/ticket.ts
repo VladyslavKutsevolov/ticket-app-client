5bd3f40e64830741807d1d5af4e52560719d42ed
Vladyslav Kutsevolov <kytsevolov@gmail.com>
2021-05-11 14:33:50 -0600

    update ticket version

--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import { Order, OrderStatus } from "./order";
+import { updateIfCurrentPlugin } from "mongoose-update-if-current";
 
 interface TicketAttr {
   id: string;
@@ -10,6 +11,7 @@ interface TicketAttr {
 export interface TicketDoc extends mongoose.Document {
   title: string;
   price: number;
+  version: number;
   isReserved(): Promise<boolean>;
 }
 
@@ -38,6 +40,8 @@ const ticketSchema = new mongoose.Schema(
     },
   }
 );
+ticketSchema.set("versionKey", "version");
+ticketSchema.plugin(updateIfCurrentPlugin);
 
 ticketSchema.statics.build = (attrs: TicketAttr) => {
   return new Ticket({
