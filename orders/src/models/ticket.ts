e9dad97b84b650919089926c973af430e59804cb
Vladyslav Kutsevolov <kytsevolov@gmail.com>
2021-05-11 15:44:29 -0600

    abstract findByEvent to ticket model

--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -17,6 +17,10 @@ export interface TicketDoc extends mongoose.Document {
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
   build: (attr: TicketAttr) => TicketDoc;
+  findByEvent: (event: {
+    id: string;
+    version: number;
+  }) => Promise<TicketDoc | null>;
 }
 
 const ticketSchema = new mongoose.Schema(
@@ -42,6 +46,16 @@ const ticketSchema = new mongoose.Schema(
 );
 ticketSchema.set("versionKey", "version");
 ticketSchema.plugin(updateIfCurrentPlugin);
+
+ticketSchema.statics.findByEvent = async (event: {
+  id: string;
+  version: number;
+}) => {
+  return Ticket.findOne({
+    _id: event.id,
+    version: event.version - 1,
+  });
+};
 
 ticketSchema.statics.build = (attrs: TicketAttr) => {
   return new Ticket({
