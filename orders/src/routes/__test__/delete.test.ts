9704d7bc471388222408c59f67525a848b10841f
Vladyslav Kutsevolov <kytsevolov@gmail.com>
2021-05-10 18:13:20 -0600

    test publish change event

--- orders/src/routes/__test__/delete.test.ts
+++ orders/src/routes/__test__/delete.test.ts
@@ -2,6 +2,7 @@ import request from "supertest";
 import { app } from "../../app";
 import { Ticket } from "../../models/ticket";
 import { Order, OrderStatus } from "../../models/order";
+import { natsWrapper } from "../../nats-wrapper";
 
 it("should marks order cancelled", async () => {
   const user = global.signin();
@@ -29,4 +30,30 @@ it("should marks order cancelled", async () => {
   const updatedOrder = await Order.findById(order.id);
 
   expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
+});
+
+it("should emits change event", async () => {
+  const user = global.signin();
+  const ticket = Ticket.build({
+    title: "adsad",
+    price: 20,
+  });
+
+  await ticket.save();
+
+  const { body: order } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", user)
+    .send({
+      ticketId: ticket.id,
+    })
+    .expect(201);
+
+  await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set("Cookie", user)
+    .send()
+    .expect(204);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
 });
