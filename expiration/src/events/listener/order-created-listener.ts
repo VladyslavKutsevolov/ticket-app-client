e2a2a34781168a905aafc94f2afe70a2c412a8bd
Vladyslav Kutsevolov <kytsevolov@gmail.com>
2021-05-12 13:42:44 -0600

    listener created

---
+++ expiration/src/events/listener/order-created-listener.ts
@@ -0,0 +1,26 @@
+import { Listener, OrderCreatedEvent, Subjects } from "@vladtickets/common";
+import { queueGroupName } from "./queueGroupName";
+import { Message } from "node-nats-streaming";
+import { expirationQueue } from "../../queues/expiration-queue";
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  subject: Subjects.OrderCreated = Subjects.OrderCreated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+    const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
+
+    console.log("delay to process job", delay);
+
+    await expirationQueue.add(
+      {
+        orderId: data.id,
+      },
+      {
+        delay,
+      }
+    );
+
+    msg.ack();
+  }
+}
