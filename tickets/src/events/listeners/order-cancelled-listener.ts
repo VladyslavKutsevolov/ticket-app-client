f05ab2547e0a023971965714d125d465b703fde8
Vladyslav Kutsevolov <kytsevolov@gmail.com>
2021-05-12 11:00:23 -0600

    order cancel listener

---
+++ tickets/src/events/listeners/order-cancelled-listener.ts
@@ -0,0 +1,33 @@
+import { Listener, OrderCancelledEvent, Subjects } from "@vladtickets/common";
+import { queGroupName } from "./queGroupName";
+import { Message } from "node-nats-streaming";
+import { Ticket } from "../../models/ticket";
+import { TicketUpdatePublisher } from "../publishers/ticket-update-publisher";
+
+export class OrderCancelledListener extends Listener<OrderCancelledEvent> {
+  subject: Subjects.OrderCancelled = Subjects.OrderCancelled;
+  queueGroupName = queGroupName;
+
+  async onMessage(data: OrderCancelledEvent["data"], msg: Message) {
+    const ticket = await Ticket.findById(data.ticket.id);
+
+    if (!ticket) {
+      throw new Error("Ticket not found");
+    }
+
+    ticket.set({ orderId: undefined });
+
+    await ticket.save();
+
+    await new TicketUpdatePublisher(this.client).publish({
+      id: ticket.id,
+      orderId: ticket.orderId,
+      userId: ticket.userId,
+      price: ticket.price,
+      title: ticket.title,
+      version: ticket.version,
+    });
+
+    msg.ack();
+  }
+}
