557689eb1cf174ae86fe39d6a87d3f2a95f79f04
Vladyslav Kutsevolov <kytsevolov@gmail.com>
2021-05-13 12:45:40 -0600

    created listener

---
+++ payments/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,23 @@
+import { Listener, OrderCreatedEvent, Subjects } from "@vladtickets/common";
+import { queueGroupName } from "./queueGroupName";
+import { Message } from "node-nats-streaming";
+import { Order } from "../../model/Order";
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  subject: Subjects.OrderCreated = Subjects.OrderCreated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+    const order = Order.build({
+      id: data.id,
+      price: data.ticket.price,
+      status: data.status,
+      userId: data.userId,
+      version: data.version,
+    });
+
+    await order.save();
+
+    msg.ack();
+  }
+}
