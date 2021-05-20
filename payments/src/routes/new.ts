2cdcd98524f905efa3ee9d2d062fb62f4f6dff5f
Vladyslav Kutsevolov <kytsevolov@gmail.com>
2021-05-20 14:01:38 -0600

    payment creation route

---
+++ payments/src/routes/new.ts
@@ -0,0 +1,48 @@
+import express, { Request, Response } from "express";
+import {
+  BadRequestError,
+  NotAuthorizedErr,
+  NotFoundError,
+  OrderStatus,
+  requireAuth,
+  validateRequest,
+} from "@vladtickets/common";
+import { body } from "express-validator";
+import { Order } from "../model/Order";
+import { stripe } from "../stripe";
+
+const router = express.Router();
+
+router.post(
+  "/api/payments",
+  requireAuth,
+  [body("token").not().isEmpty().withMessage("Token must be present")],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { token, orderId } = req.body;
+
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedErr();
+    }
+
+    if (order.status === OrderStatus.Cancelled) {
+      throw new BadRequestError("Cannot pay for cancelled order");
+    }
+
+    await stripe.charges.create({
+      currency: "usd",
+      amount: order.price * 100,
+      source: token,
+    });
+
+    res.status(201).send({ success: true });
+  }
+);
+
+export { router as paymentNewRouter };
